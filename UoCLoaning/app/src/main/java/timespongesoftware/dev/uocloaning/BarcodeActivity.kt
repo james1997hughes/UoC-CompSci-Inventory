1fe930536012bc54aca3bc149edf8df3bd4fbea5
James Hughes <james1997hughes@gmail.com>
2019-05-10 11:04:12 +0100

    Camera support, focus and reconnect after pause

--- UoCLoaning/app/src/main/java/timespongesoftware/dev/uocloaning/BarcodeActivity.kt
+++ UoCLoaning/app/src/main/java/timespongesoftware/dev/uocloaning/BarcodeActivity.kt
@@ -13,31 +13,61 @@ class BarcodeActivity : AppCompatActivity(), SurfaceHolder.Callback {
     lateinit var surfaceview: SurfaceView
     lateinit var surfaceholder: SurfaceHolder
     lateinit var backCamera: Camera
+    lateinit var camParams: Camera.Parameters
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barcode)
-        surfaceview = camPrevSV
-        surfaceholder = surfaceview.holder
-        surfaceholder.addCallback(this)
-        surfaceholder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+        setupSurface()
         backCamera = getCamera()
-
+        setParameters()
     }
 
+    fun setupSurface(){
+        try {
+            surfaceview = camPrevSV
+            surfaceholder = surfaceview.holder
+            surfaceholder.addCallback(this)
+            surfaceholder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+        } catch(E: Exception){
+        }
+    }
+    fun setParameters(){
+        try {
+            camParams = backCamera.parameters
+            camParams.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
+            backCamera.parameters = camParams
+        } catch (E: Exception){
+            Log.d("error", "Error setting camera parameters")
+        }
+    }
     fun getCamera(): Camera {
 
             return Camera.open()
-    
+
+
+
     }
 
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        backCamera?.startPreview()
+
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        backCamera = getCamera()
+
+
 
     }
 
     override fun surfaceCreated(holder: SurfaceHolder?) {
         try{
+            backCamera.setDisplayOrientation(90)
             backCamera.setPreviewDisplay(surfaceholder)
             backCamera.startPreview()
         } catch(E: Exception){
@@ -46,6 +76,12 @@ class BarcodeActivity : AppCompatActivity(), SurfaceHolder.Callback {
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        try {
+            backCamera.stopPreview()
+            backCamera.release()
+        } catch(E: Exception){
+            Log.d("error", "couldn't release camera")
+        }
 
     }
 
