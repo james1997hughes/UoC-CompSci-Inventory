760de81e30fbb010d59a3eb2aca633ea8f49ca25
James Hughes <james1997hughes@gmail.com>
2019-05-13 13:02:28 +0100

    QR processing added, toast added temporarily

--- UoCLoaning/app/src/main/java/timespongesoftware/dev/uocloaning/BarcodeActivity.kt
+++ UoCLoaning/app/src/main/java/timespongesoftware/dev/uocloaning/BarcodeActivity.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import android.hardware.Camera
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.camprev_barcode.*
 
-class BarcodeActivity : AppCompatActivity(), SurfaceHolder.Callback {
-    lateinit var surfaceview: SurfaceView
-    lateinit var surfaceholder: SurfaceHolder
-    lateinit var backCamera: Camera
-    lateinit var camParams: Camera.Parameters
+class BarcodeActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.PreviewCallback {
+    private lateinit var surfaceview: SurfaceView
+    private lateinit var surfaceholder: SurfaceHolder
+    private lateinit var backCamera: Camera
+    private lateinit var camParams: Camera.Parameters
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,43 @@ class BarcodeActivity : AppCompatActivity(), SurfaceHolder.Callback {
         backCamera = getCamera()
         setParameters()
     }
+
+    private val imageProcesser = ProcessImage()
+    var frameDelay = 0
+    var toasting = false
+    override fun onPreviewFrame(data: ByteArray, camera: Camera?) {
+        //Only run detection every 10 frames - 6 times per second on a 60fps camera
+        //Saves a fuck ton of processing waste
+        //Weirdly need to move camera around a bit for it to pass through properly - issue with this method of vision library???
+        //In real world use case, probably not a problem, hand shake etc, still very fast at scanning it
+
+            frameDelay++
+        if (!toasting) {
+            if (frameDelay == 1) {
+                var barcode = imageProcesser.barcodeFromCamera(
+                    data,
+                    backCamera.parameters.previewSize.width,
+                    backCamera.parameters.previewSize.height
+                )
+                if (imageProcesser.hasBeenSuccessful() && barcode!="" && barcode != null){
+                    toasting = true
+                    tellUser(imageProcesser.passBarcodeBack())
+
+                }
+            } else if (frameDelay == 10) {
+                frameDelay = 0
+            }
+        }
+    }
+  fun tellUser(textToShow: String?){
+        Log.d("test", textToShow)
+        val toast = Toast.makeText(applicationContext, textToShow, Toast.LENGTH_SHORT)
+        toast.show()
+        toasting = false
+
+    }
+
+
 
     fun setupSurface(){
         try {
@@ -42,33 +82,25 @@ class BarcodeActivity : AppCompatActivity(), SurfaceHolder.Callback {
         }
     }
     fun getCamera(): Camera {
-
             return Camera.open()
-
-
-
     }
 
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
         backCamera?.startPreview()
-
-
-
     }
 
     override fun onResume() {
         super.onResume()
         backCamera = getCamera()
-
-
-
     }
 
     override fun surfaceCreated(holder: SurfaceHolder?) {
         try{
+            setParameters()
             backCamera.setDisplayOrientation(90)
             backCamera.setPreviewDisplay(surfaceholder)
+            backCamera.setPreviewCallback(this)
             backCamera.startPreview()
         } catch(E: Exception){
             Log.d("error", "couldnt start preview")
